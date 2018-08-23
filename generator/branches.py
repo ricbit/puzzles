a70ce6abfbce6277fb5e514700955fc65c657b8f
Ricardo Bittencourt <bluepenguin@gmail.com>
2018-08-23 12:35:14 -0300

    Refactor second iterator in branches generator

--- generator/branches.py
+++ generator/branches.py
@@ -6,7 +6,7 @@ h = int(sys.argv[2])
 
 grid = [[-1] * w for _ in xrange(h)]
 
-def iter_empty(grid):
+def iter_empty_cells(grid):
   pos = 0
   for j in xrange(h):
     for i in xrange(w):
@@ -16,41 +16,44 @@ def iter_empty(grid):
 
 def count(grid):
   ans = 0
-  for j, i, pos in iter_empty(grid):
+  for j, i, pos in iter_empty_cells(grid):
     ans = pos + 1
   return ans
 
 def find(grid, x):
-  for j, i, pos in iter_empty(grid):
+  for j, i, pos in iter_empty_cells(grid):
     if x == pos:
       return (j, i)
   return None
 
 alld = [(1,0), (0,1), (-1,0), (0,-1)]
 
-def traverse(grid, j, i, d):
+def iter_direction(grid, j, i, d):
   di, dj = d
   cur = 0
   ii, jj = i + di, j + dj
   while ii >= 0 and jj >= 0 and ii < w and jj < h and grid[jj][ii] == -1:
+    yield (jj, ii, cur)
     cur += 1
     ii += di
     jj += dj
-  return cur
+
+def traverse(grid, j, i, d):
+  ans = 0
+  for jj, ii, cur in iter_direction(grid, j, i, d):
+    ans = cur + 1
+  return ans
 
 def fill(grid, j, i, d, left):
   if left == 0:
     return 0
   tmax = min(left, random.randint(0, traverse(grid, j, i, d)))
-  di, dj = d
-  cur = 0
-  ii, jj = i + di, j + dj
-  while ii >= 0 and jj >= 0 and ii < w and jj < h and grid[jj][ii] == -1 and cur <= tmax:
-    grid[jj][ii] = -2
-    cur += 1
-    ii += di
-    jj += dj
-  return cur
+  ans = 0
+  for jj, ii, cur in iter_direction(grid, j, i, d):
+    if cur <= tmax:
+      grid[jj][ii] = -2
+      ans = cur + 1
+  return ans
 
 g = 0
 while True:
