040e0a2203040c93eccdaa0feb50cd0bbf2f75e3
Ricardo Bittencourt <bluepenguin@gmail.com>
2019-01-28 22:32:52 -0200

    Torto using Partial Cover

--- torto.dlx2.py
+++ torto.dlx2.py
@@ -1,5 +1,14 @@
 # Takes a list of words and build a torto model to be used with Knuth's dlx2 solver.
-# Usage: python torto.dlx2.py < words.txt > model.dlx
+#
+# If you want to use plain dlx2:
+#   python torto.dlx2.py < words.txt > model.dlx
+#
+# If you want to use "Partial Cover":
+#   SHARP=1 python torto.dlx2.py < words.txt > model.dlx
+
+import os
+import sys
+from collections import Counter
 
 def iter_torto():
   for j in xrange(6):
@@ -80,20 +89,57 @@ def get_symmetry(n, i, size):
     return id_generator
 
 def collect_word(n, word):
+  sharp = "#" if os.getenv("SHARP") is not None else ""
   for i in xrange(len(word) - 1):
     a, b = w = word[i: i + 2]
     transform = get_symmetry(n, i, len(word))
     for name, pa, pb in transform(flip(iter_torto())):
-      yield "W%d_%d %s %s p%s:%s p%s:%s %spw%d_%s:%s pw%d_%s:%s" % (
-        n, i, item_letter(n, i, pa), item_letter(n, i + 1, pb),
+      yield "%sW%d_%d %s %s p%s:%s p%s:%s %spw%d_%s:%s pw%d_%s:%s" % (
+        sharp, n, i, item_letter(n, i, pa), item_letter(n, i + 1, pb),
         pos(pa), a, pos(pb), b, diag(n, name, pa, pb),
         n, encode_pos(pa), encode(i), n, encode_pos(pb), encode(i + 1))
 
+def histogram(words):
+  global_hist = Counter()
+  for word in words:
+    local_hist = Counter()
+    for item in word:
+      local_hist[item] += 1
+    for item, count in local_hist.iteritems():
+      global_hist[item] = max(global_hist[item], count)
+  return global_hist
+
+def collect_letters(words):
+  letters = histogram(words)
+  for letter, count in letters.iteritems():
+    for c in xrange(count):
+      for j in xrange(6):
+        for i in xrange(3):
+          p = (j, i)
+          yield "L%s%d p%s:%s l%s%d:%s" % (
+              letter, c, pos(p), letter, letter, c, encode_pos(p))
+
+def word_bigrams(words):
+  for word in words:
+    bigrams = [list(i) for i in zip(word, word[1:])]
+    for bigram in bigrams:
+      bigram.sort()
+    yield ["".join(bigram) for bigram in bigrams]
+
+def collect_bigrams(words):
+  bigrams = histogram(word_bigrams(words))
+  edges = flip(iter_torto())
+  for bigram, count in bigrams.iteritems():
+    for edge in flip(iter_torto()):
+      yield "B%s%d p%s:%s p%s:%s b%s%d_0:%s b%s%d_1:%s" % (
+          bigram, count, pos(edge[1]), bigram[0], pos(edge[2]), bigram[1],
+          bigram, count, encode_pos(edge[1]), bigram, count, encode_pos(edge[2]))
+
 def extract_primary(option):
   for item in option.split():
-    if ":" not in item and item[0].isupper():
+    if ":" not in item and (item[0].isupper() or item[0] == "#"):
       yield item
-      
+
 def extract_secondary(option):
   for item in option.split():
     if ":" in item or item[0].islower():
@@ -105,6 +151,9 @@ words.sort(key=lambda w: len(w), reverse=True)
 options = []
 for n, word in enumerate(words):
   options.extend(collect_word(n, word))
+if os.getenv("SHARP") is not None:
+  options.extend(collect_letters(words))
+  options.extend(collect_bigrams(words))
 primary = set()
 secondary = set()
 for option in options:
